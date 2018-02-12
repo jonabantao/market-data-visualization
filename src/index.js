8bee5f03484f8ea9035b95aa8eeb7f797726de61
Jonathan Abantao <jonabantao@gmail.com>
2018-02-12 14:55:42 -0800

    Upload initial layout - begin exploring D3 graphs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import * as d3 from 'd3';
+// https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb&types=quote,news,chart,earnings&range=1y&last=3
+// ^^ Request to be used to extract data
+// d3.json(url, (error, response))
+
 
 window.d3 = d3;
+
